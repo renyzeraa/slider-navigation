0b746339860494b2f9758cb391502b19d9214dc2
Renan Silva <renansilvaytB@gmail.com>
2024-02-03 18:52:18 -0300

    feat touch events

--- js/slide.js
+++ js/slide.js
@@ -14,34 +14,68 @@ export default class Slide {
     }
   }
 
+  isMobileDevice() {
+    return (
+      'ontouchstart' in window ||
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    )
+  }
+
+  /**
+   * Updates the position of the slide based on the current mouse position.
+   * @param {number} iClientX
+   */
   updatePosition(iClientX) {
     this.dist.movement = (this.dist.startX - iClientX) * 1.3
     return this.dist.finalPosition - this.dist.movement
   }
 
+  /**
+   * Moves the slide by the specified distance.
+   * @param {number} distX
+   */
   moveSlide(distX) {
     this.dist.movePosition = distX
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
   }
 
+  /**
+   * Inicia evento de movimento no slide
+   * @param {Event} oEv
+   */
   onStart = oEv => {
     oEv.preventDefault()
-    this.dist.startX = oEv.clientX
-    this.wrapper.addEventListener('mousemove', this.onMove)
+    if (this.isMobileDevice()) {
+      this.dist.startX = oEv.changedTouches[0].clientX
+    } else {
+      this.dist.startX = oEv.clientX
+    }
+    this.wrapper.addEventListener(
+      this.isMobileDevice() ? 'touchmove' : 'mousemove',
+      this.onMove
+    )
   }
 
   onMove = oEv => {
-    const finalPosition = this.updatePosition(oEv.clientX)
+    const iPosX = this.isMobileDevice()
+      ? oEv.changedTouches[0].clientX
+      : oEv.clientX
+    const finalPosition = this.updatePosition(iPosX)
     this.moveSlide(finalPosition)
   }
 
-  onEnd = oEv => {
-    this.wrapper.removeEventListener('mousemove', this.onMove)
+  onEnd = () => {
+    const sMoveType = this.isMobileDevice() ? 'touchmove' : 'mousemove'
+    this.wrapper.removeEventListener(sMoveType, this.onMove)
     this.dist.finalPosition = this.dist.movePosition
   }
 
   addSlideEvents = () => {
     this.wrapper.addEventListener('mousedown', this.onStart)
+    this.wrapper.addEventListener('touchstart', this.onStart)
+    this.wrapper.addEventListener('touchend', this.onEnd)
     this.wrapper.addEventListener('mouseup', this.onEnd)
   }
 
