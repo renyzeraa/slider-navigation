96074a2023f93f6debf188da9e74df814e11e647
Renan Silva <renansilvaytB@gmail.com>
2024-02-03 17:50:28 -0300

    feat Calculates distance as events occur

--- js/slide.js
+++ js/slide.js
@@ -6,17 +6,38 @@ export default class Slide {
   constructor(oSlide, oWrapper) {
     this.slide = oSlide
     this.wrapper = oWrapper
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+      movePosition: 0
+    }
+  }
+
+  updatePosition(iClientX) {
+    this.dist.movement = (this.dist.startX - iClientX) * 1.3
+    return this.dist.finalPosition - this.dist.movement
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
   }
 
   onStart = oEv => {
     oEv.preventDefault()
+    this.dist.startX = oEv.clientX
     this.wrapper.addEventListener('mousemove', this.onMove)
   }
 
-  onMove = oEv => {}
+  onMove = oEv => {
+    const finalPosition = this.updatePosition(oEv.clientX)
+    this.moveSlide(finalPosition)
+  }
 
   onEnd = oEv => {
     this.wrapper.removeEventListener('mousemove', this.onMove)
+    this.dist.finalPosition = this.dist.movePosition
   }
 
   addSlideEvents = () => {
